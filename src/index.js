c94c702e7601efadf55b949510ed8f8586b3039c
henrycky <henrycky_wottikosky@hotmail.com>
2022-06-01 10:31:34 -0300

    feature/cadastro do cliente

--- src/index.js
+++ src/index.js
@@ -1,50 +1,43 @@
 const express = require('express');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
 app.use(express.json());
 
-/**
- * GET - Buscar uma informação dentro do servidor
- * POST - Inserir uma informação no servidor
- * PUT - Alterar uma informação no servidor
- * PATCH - Alterar uma informação especificado no
- * DELETE - Deletar uma informação no servidor
- */
+const customers = [];
 
 /**
- * Tipos de parâmetros
- * 
- * Route Params => Identificar um recurso: editar/deletar/buscar
- * Query Params => Paginação / Filtro
- * Body Params => Os objetos inserção/alteração (JSON)
- */
+ * O que é preciso pra criar uma conta?
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement []
+*/
 
-app.get("/courses", (request, response) => {
-    const query = request.query;
-    console.log(query);
-    return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+
+    /**Verificando se o cpf a ser cadastrado já existe ou não, pois uma das
+     *  regras de negócio não permite cadastro de cpf repetido */
+
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+
+    if(customerAlreadyExists){
+        return response.status(400).json({ error: "Customer already exists!" })
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    })
+
+    return response.status(201).send();
+
 });
-
-app.post("/courses", (request, response) => {
-    const body = request.body;
-    console.log(body);
-    return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
-});
-
-app.put("/courses/:id", (request, response) => {
-    const {id} = request.params;
-    console.log(id);
-    return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
-});
-
-app.patch("/courses/:id", (request, response) => {
-    return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
-});
-
-app.delete("/courses/:id", (request, response) => {
-    return response.json(["Curso 6", "Curso 7", "Curso 4"]);
-});
-
 
 app.listen(3333);
